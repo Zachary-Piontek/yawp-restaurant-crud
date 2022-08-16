3a1369872b833d577e7be178f4f51a386dc1ebf6
Zachary Piontek <fortunefavorsthebrave@protonmail.com>
2022-08-16 16:41:37 -0700

    test and review add for controller and model added

--- __tests__/restaurant.test.js
+++ __tests__/restaurant.test.js
@@ -14,20 +14,34 @@ describe('backend-express-template routes', () => {
     expect(res.body.length).toEqual(4);
   });
 
-  it('/restaurants:id returns a restaurant with a review', async () => {
+  it('/restaurants/:id returns a restaurant with a review', async () => {
     const resp = await request(app).get('/api/v1/restaurants/1');
-    const restaurantOne = {
-      review: {
+    const restaurantOne = {      
+      food: 'American',
+      id: '1',
+      name: 'McDonalds',
+      reviews: [{
         id: 1,
-        name: 'McDonalds',
-        service: 'Fast-Food',
-        food: 'American'
-      },
-      id: 1,
-      rating: '3 stars',
-      opinion: 'definitely got the food fast but does not look anything like in commercials'
+        rating: '3 stars',
+        opinion: 'definitely got the food fast but does not look anything like in commercials'
+      }],
+      service: 'Fast-Food',
     };
+
     expect(resp.body).toEqual(restaurantOne);
+  });
+
+  it('/restaurants/:id/reviews create new review', async () => {
+    const resp = await request(app)
+      .post('/api/v1/restaurants/1/reviews')
+      .send({ rating: '5 stars', opinion: 'Great every, single, time' });
+    expect(resp.body).toEqual({
+      books: expect.any(Array),
+      id: '1',
+      rating: '5 stars',
+      opinion: 'Great every, single, time',
+    });
+    console.log(resp.body);
   });
 
   afterAll(() => {
